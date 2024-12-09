78f1fa42264e7ec38d74e929ebd646115cb3a962
Kanishka-Trivedi <kally651997@gmail.com>
2024-12-09 10:31:34 +0530

    Initial Commit

---
+++ pract.js
@@ -0,0 +1,31 @@
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(response => response.json())  // Convert response to JSON
+//   .then(data => console.log(data))    // Handle the data
+//   .catch(error => console.error('Error:', error)); 
+
+// const axios = require('axios'); // Import Axios if using Node.js
+
+// axios.get("https://jsonplaceholder.typicode.com/posts")
+//   .then(response => {
+//     console.log(response.data); // Handle the data (JSON is automatically parsed)
+//   })
+//   .catch(error => {
+//     console.error('Error:', error); // Handle the error
+//   });
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(response => response.json())  // Convert response to JSON
+//   .then(data =>  {
+// // Select the first user's data
+// const user = data[0]; 
+
+// // Display the data using innerHTML
+// document.getElementById('data').innerHTML = `
+//  <h2>User Post</h2>
+//  <p>User Id: ${user.userId}</p>
+//  <p>Id: ${user.id}</p>
+//  <p>Title: ${user.title}</p>
+//  <p>Body: ${user.body}</p>
+// `;
+// });
